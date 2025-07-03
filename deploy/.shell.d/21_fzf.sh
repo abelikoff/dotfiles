812a55cb2b18bece874a33b8a4ea61f3eb60e7eb
Alexander L. Belikoff <1753146+abelikoff@users.noreply.github.com>
2025-07-03 00:16:05 -0400

    fzf support for all 3 generations of fzf configuration approaches.

--- deploy/.shell.d/21_fzf.sh
+++ deploy/.shell.d/21_fzf.sh
@@ -1,6 +1,36 @@
 # fzf setup
 
-if [ -n "$(which fzf)" ]; then
+if command -v fzf-share >/dev/null; then
+    if [ -n "$BASH_VERSION" ]; then
+        source "$(fzf-share)/key-bindings.bash"
+        source "$(fzf-share)/completion.bash"
+
+    elif [ -n "$ZSH_VERSION" ]; then
+        source "$(fzf-share)/key-bindings.zsh"
+        source "$(fzf-share)/completion.zsh"
+    fi
+
+elif [[ -d /usr/share/doc/fzf/examples ]]; then
+    if [ -n "$ZSH_VERSION" ]; then
+        if [ -f /usr/share/doc/fzf/examples/key-bindings.zsh ]; then
+            source /usr/share/doc/fzf/examples/key-bindings.zsh
+        fi
+
+        if [ -f /usr/share/doc/fzf/examples/completion.zsh ]; then
+            source /usr/share/doc/fzf/examples/completion.zsh
+        fi
+
+    elif [ -n "$BASH_VERSION" ]; then
+        if [ -f /usr/share/doc/fzf/examples/key-bindings.bash ]; then
+            source /usr/share/doc/fzf/examples/key-bindings.bash
+        fi
+
+        if [ -f /usr/share/doc/fzf/examples/completion.bash ]; then
+            source /usr/share/doc/fzf/examples/completion.bash
+        fi
+    fi
+
+elif [ -n "$(which fzf)" ]; then   # old way
     if [ -n "$ZSH_VERSION" ]; then
         source <(fzf --zsh)
     elif [ -n "$BASH_VERSION" ]; then
