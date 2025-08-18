8cee019304cfe28b8ec5f9059f2639668d99342a
Alexander L. Belikoff <1753146+abelikoff@users.noreply.github.com>
2025-08-18 14:34:46 -0400

    Updated Rofi config.

--- deploy/.config/rofi/scripts/powermenu.sh
+++ deploy/.config/rofi/scripts/powermenu.sh
@@ -9,13 +9,14 @@ options=" Lock\n Logout\n Suspend\n Reboot\n Shutdown"
 # -dmenu: Run rofi in dmenu mode.
 # -p: Set a prompt.
 # -theme: Use a custom theme file.
-selected_option=$(echo -e "$options" | rofi -dmenu -p "Power" -theme ~/.config/rofi/themes/powermenu.rasi)
+#selected_option=$(echo -e "$options" | rofi -dmenu -p "Power" -theme ~/.config/rofi/themes/powermenu.rasi)
+selected_option=$(echo -e "$options" | rofi -dmenu -config ~/.config/rofi/powermenu.rasi)
 
 # Execute a command based on the selected option.
 case $selected_option in
 " Lock")
-    # Use i3lock or any other screen locker you have configured.
-    i3lock-color -c 000000
+    #i3lock-color -c 000000
+    xscreensaver-command -lock
     ;;
 " Logout")
     # Log out of the i3 session.
